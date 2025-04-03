bb114eca84272e7b7ffb38101bd5a66a6b594c6c
Omer Hershkovitz <hershkovitzomer@gmail.com>
2025-04-04 00:15:16 +0300

    update diffview and lualine plugins

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,6 +1,20 @@
 return {
   'sindrets/diffview.nvim',
-  opts = {},
+  opts = {
+    keymaps = {
+      file_panel = {
+        {
+          'n',
+          'e',
+          function()
+            require('diffview.actions').goto_file_edit()
+          end,
+          { desc = 'Open the file in the previous tabpage' },
+        },
+        { 'n', 'q', '<cmd>DiffviewClose<cr>', desc = 'Close Diffview' },
+      },
+    },
+  },
   keys = {
     { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[g]it [d]iff' },
     { '<leader>gc', '<cmd>DiffviewClose<cr>', desc = '[g]it [c]lose' },
